8a0b4efa7320a3e61c73d7b06e12fe4d17b90c58
nahidulhasan4 <nahidhasan4836@gmail.com>
2025-11-16 11:05:52 -0800

    here add navber codiing and add nav li

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,16 @@ const Navbar = () => {
             Nahid &nbsp; <span className='sm:block hidden'>|  Portfolio</span>
           </p>
         </Link>
-        <p className='text-red-500'>assas</p>
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+          {navLinks.map((link) => (
+            <li key={link.id} 
+            className={`${active === link.title ? "text-white" : "text-secondary"} 
+            hover:text-white text-[18px] font-medium cursor-pointer`}
+            onClick={()=> setActive(link.title)}>
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
        </div>
        
     </nav>
