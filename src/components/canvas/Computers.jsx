5062728a0f1fc20b785edaf8e223ba99ef9ba4e1
nahidulhasan4 <nahidhasan4836@gmail.com>
2025-11-18 04:11:21 -0800

    here add 3d model for website loader sport loader is not woirkig r

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -7,8 +7,14 @@ const Computers = () => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
-      <pointLight intensity={3} />
+      <hemisphereLight intensity={0.15} groundColor="red" />
+      <pointLight intensity={3} />      
+      <primitive
+        object={computer.scene}
+        scale={0.75}
+        position={[0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
@@ -16,12 +22,6 @@ const Computers = () => {
         intensity={1}
         castShadow
         shadow-mapSize={1024}
-      />
-      <primitive
-        object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
   );
