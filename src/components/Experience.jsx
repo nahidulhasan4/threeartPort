06703038e0463317963dcbf5a5b5d43866b081c9
nahidulhasan4 <nahidhasan4836@gmail.com>
2025-11-24 09:57:54 -0800

    here exprience start here created component around finised

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+ import {motion} from 'framer-motion';
+
 import{ VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component'
 import 'react-vertical-timeline-component/style.min.css' ;
-import motion from 'framer-motion';
 import { styles } from '../styles';
 import { experiences } from '../constants';
 import { SectionWrapper } from '../hoc';
