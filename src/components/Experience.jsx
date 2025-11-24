3c9652e6c93eee7179c6e51fd1136d327faf1e2a
nahidulhasan4 <nahidhasan4836@gmail.com>
2025-11-24 10:03:26 -0800

    here exprience section wraper not worikng

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -10,7 +10,7 @@ import { textVariant } from '../utils/motion';
 
 const Experience = () => {
   return (
-    <motion.div variants={textVariant()}>
+    <motion.div variants={textVariant( )}>
       <p className={styles.sectionSubText}>What I have done so far</p>
       <h2 className={styles.sectionHeadText}>Work Experience.</h2>
       <div className='mt-20 flex flex-col ' >
@@ -56,4 +56,4 @@ const Experience = () => {
   )
 }
 
-export default Experience
+export default SectionWrapper(Experience, "work");
