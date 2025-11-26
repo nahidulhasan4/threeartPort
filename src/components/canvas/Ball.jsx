5960089d782cc4e457d81de46e2f2c6014dd4f7d
nahidulhasan4 <nahidhasan4836@gmail.com>
2025-11-26 09:46:12 -0800

    here import items for work on tech ball icon fixing

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,34 +1,58 @@
-import React, {Suspense} from 'react';
-import { Canvas } from '@react-three/fiber';
-import { Decal,Float, OrbitControls, Preload, useTexture } from '@react-three/drei'  ;
-import CanvasLoader from '../Loader'; // লোডার কম্পোনেন্ট আমদানি করা
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import {
+  Decal,
+  Float,
+  OrbitControls,
+  Preload,
+  useTexture,
+} from "@react-three/drei";
+import CanvasLoader from "../Loader"; // লোডার কম্পোনেন্ট আমদানি করা
 
-// Ball কম্পোনেন্ট - একটি 3D বল তৈরি করে যা একটি আইকন প্রদর্শন করে
-const Ball = ({icon}) => {
-  // useTexture হুক ব্যবহার করে আইকনের টেক্সচার লোড করা
-  // const [decal] = useTexture([icon]); 
+const Ball = (props) => {
+  const [Decal] = useTexture([props.ImgUrl]);
+
   return (
     <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
       <ambientLight intensity={0.25} />
-      <directionalLight position={[0,0,0.05]} />
+      <directionalLight position={[0, 0, 0.05]} />
       <mesh castShadow receiveShadow scale={2.75}>
-        <icosahedronGeometry args={[1,1]} />
+        <icosahedronGeometry args={[1, 1]} />
         <meshStandardMaterial
           color="#fff8eb"
           polygonOffset
           polygonOffsetFactor={-5}
           flatShading
         />
-        <Decal 
-          position={[0,0,1]} 
-          rotation={[0,0,0]}
+        <Decal
+          position={[0, 0, 1]}    
+          rotation={[2 * Math.PI, 0, 6.25]}
           scale={1}
-          // map={decal} 
+          map={Decal}
         />
       </mesh>
+      
     </Float>
   )
-}
+};
+const BallCanvas = ({ icon }) => {
+  return (
+   
+      <Canvas
+      frameloop="demand"
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Ball ImgUrl={icon} />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
 
-
-export default Ball
+export default BallCanvas;
