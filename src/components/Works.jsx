5fea4bd2ae8a83652b5fe8273a4a465abe4054d9
nahidulhasan4 <nahidhasan4836@gmail.com>
2025-11-26 10:07:34 -0800

    here start work on work section

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,4 +1,11 @@
 import React from 'react'
+import { motion } from "framer-motion"; // অ্যানিমেশন লাইব্রেরি
+import { SectionWrapper } from "../hoc"; // সেকশন র্যাপার হোক
+
+/**
+ * Works - কাজের সেকশন কম্পোনেন্ট
+ * এটি বিভিন্ন প্রকল্প বা কাজের প্রদর্শন করে
+ */
 
 const Works = () => {
   return (
