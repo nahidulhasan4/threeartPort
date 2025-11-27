0f5e411d5dbaa439e7f8c081a81406eeec110191
nahidulhasan4 <nahidhasan4836@gmail.com>
2025-11-27 10:44:06 -0800

    here start work on work section imported libeary

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { motion } from "framer-motion"; // অ্যানিমেশন লাইব্রেরি
 import { SectionWrapper } from "../hoc"; // সেকশন র্যাপার হোক
 import Tilt from "react-tilt"; // টিল্ট এফেক্টের জন্য লাইব্রেরি
+import { github } from '../assets'; //
 import { styles } from "../styles"; // গ্লোবাল স্টাইলস
 import { projects } from "../constants"; // প্রকল্পের ডেটা
 import { fadeIn, textVariant } from "../utils/motion"; // অ্যানিমেশন ভ্যারিয়েন্ট 
