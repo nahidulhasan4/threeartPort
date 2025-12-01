30646ecf943c96260d34e9e847a55e4bb4c8a629
nahidulhasan4 <nahidhasan4836@gmail.com>
2025-12-01 10:21:33 -0800

    here start working on comntact sectiion

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Canvas } from '@react-three/fiber';
+import { Suspense } from 'react';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import CanvasLoader from '../Loader';
+
 
 const Earth = () => {
   return (
     <div>Earth</div>
   )
 }
+
+const EarthCanvas = () => {
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [4, 3, 6], fov: 50 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false} />
+        <Earth />
+      </Suspense> 
+      <Preload all />
+    </Canvas>
+  )
+} 
 
 export default Earth
