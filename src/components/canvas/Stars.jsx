c68a13f3df732b73202d67d8638999fe7cfe882c
nahidulhasan4 <nahidhasan4836@gmail.com>
2025-12-02 09:47:15 -0800

    here start working on contact from fuctionalty

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -3,12 +3,45 @@ import { Canvas, useFrame } from '@react-three/fiber';
 import { points, PointMaterial, Preload } from '@react-three/drei';
 import * as random from 'maath/random/dist/maath-random.esm';
 
-const Stars = () => {
+const Stars = (props) => {
+  const ref = useRef();
+  const [sphere] = useState(() =>
+    random.inSphere(new Float32Array(5000), { radius: 1.2 })
+  );
+  useFrame((state, delta) => {
+    ref.current.rotation.x -= delta / 10;
+    ref.current.rotation.y -= delta / 15;
+  });
+
   return (
-    <div>Stars</div>
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <points ref={ref} positions={sphere} stride={3} frustumCulled={false} {...props} >
+        <PointMaterial
+          transparent
+          color='#f272c8'
+          size={0.002}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+        /</points>
+
+      
+    </group>
   )
 }
 const StarsCanvas = () => {
-  const starRef = useRef();
+  return (
+    <div className='w-full h-auto absolute inset-0 z-[-1]'>
+      <Canvas camera={{ position: [0, 0, 1] }}>
+        <Suspense fallback={null}>
+          <Stars />
+        </Suspense>
+        <Preload all />
+      </Canvas>
+    </div>
 
-export default Stars
+  )
+}
+
+
+export default StarsCanvas
